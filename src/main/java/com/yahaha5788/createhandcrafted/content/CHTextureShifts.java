a7ee94db56878e015d13a374c2df9fde28364f3a
yahaha5788 <j.thomps1310@gmail.com>
2025-07-31 07:30:20 -0400

    finished texture and changed ct logic

--- src/main/java/com/yahaha5788/createhandcrafted/content/CHTextureShifts.java
+++ src/main/java/com/yahaha5788/createhandcrafted/content/CHTextureShifts.java
@@ -3,33 +3,19 @@ package com.yahaha5788.createhandcrafted.content;
 import com.simibubi.create.foundation.block.connected.AllCTTypes;
 import com.simibubi.create.foundation.block.connected.CTSpriteShiftEntry;
 import com.simibubi.create.foundation.block.connected.CTSpriteShifter;
-import com.simibubi.create.foundation.block.connected.CTType;
 import com.yahaha5788.createhandcrafted.CreateHandcrafted;
 
 
 public class CHTextureShifts {
 
-    public static final CTSpriteShiftEntry GOLD_CASING = omni("gold_casing");
+    public static final CTSpriteShiftEntry GOLD_CASING = getCasingCT("gold_casing");
 
+    public static final CTSpriteShiftEntry GILDED_CASING = getCasingCT("gilded_casing");
 
-    public static CTSpriteShiftEntry omni(String name) {
-        return getCT(AllCTTypes.OMNIDIRECTIONAL, name);
+    private static CTSpriteShiftEntry getCasingCT(String casingName) {
+        return CTSpriteShifter.getCT(AllCTTypes.OMNIDIRECTIONAL,
+                CreateHandcrafted.asResource("block/" + casingName),
+                CreateHandcrafted.asResource("block/" + casingName + "_connected")
+                );
     }
-
-    public static CTSpriteShiftEntry horizontal(String name) {
-        return getCT(AllCTTypes.HORIZONTAL, name);
-    }
-
-    public static CTSpriteShiftEntry vertical(String name) {
-        return getCT(AllCTTypes.VERTICAL, name);
-    }
-
-    private static CTSpriteShiftEntry getCT(CTType type, String blockTextureName, String connectedTextureName) {
-        return CTSpriteShifter.getCT(type, CreateHandcrafted.asResource("block/" + blockTextureName), CreateHandcrafted.asResource("block/" + connectedTextureName + "_connected"));
-    }
-
-    private static CTSpriteShiftEntry getCT(CTType type, String blockTextureName) {
-        return getCT(type, blockTextureName, blockTextureName);
-    }
-
 }
